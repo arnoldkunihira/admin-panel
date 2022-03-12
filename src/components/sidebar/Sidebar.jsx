1a5543f89b71461af24c40e6fa8ded5cdae0ea32
Arnold Kunihira <arnold.kmulera@gmail.com>
2022-03-12 10:31:35 +0300

    Modify empty tag for color options

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -79,8 +79,8 @@ const Sidebar = () => {
                 </ul>
             </div>
             <div className="bottom">
-                <div className="colorOption"></div>
-                <div className="colorOption"></div>
+                <div className="colorOption" />
+                <div className="colorOption" />
             </div>
         </div>
     );
