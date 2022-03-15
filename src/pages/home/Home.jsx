0efbda2ffceb03c2acf1816acea04b98d7135aed
Arnold Kunihira <arnold.kmulera@gmail.com>
2022-03-15 23:33:43 +0300

    Add props to the chart component on home page

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -23,7 +23,7 @@ const Home = () => {
 
                 <div className="charts">
                     <Featured />
-                    <Chart />
+                    <Chart aspect={2} title="Last 6 Months (Revenue)" />
                 </div>
                 <div className="listContainer">
                     <div className="listTitle">
