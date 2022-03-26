57e8a87d76a74ead78299c8affbb917e3d1685d9
Arnold Kunihira <arnold.kmulera@gmail.com>
2022-03-26 18:40:57 +0300

    Enclose the entire app with a dark context provider

--- src/index.js
+++ src/index.js
@@ -2,10 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
+import { DarkModeContextProvider } from "./context/darkModeContext";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+      <DarkModeContextProvider>
+          <App />
+      </DarkModeContextProvider>
   </React.StrictMode>,
   document.getElementById("root")
 );
