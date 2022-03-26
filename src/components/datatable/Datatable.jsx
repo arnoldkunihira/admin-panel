c33a647c0dc5c09abc8e692e2e440c99d8225240
Arnold Kunihira <arnold.kmulera@gmail.com>
2022-03-26 18:34:24 +0300

    Modify datatables with logic when deleting a user

--- src/components/datatable/Datatable.jsx
+++ src/components/datatable/Datatable.jsx
@@ -1,22 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 import "./datatable.scss";
 import { DataGrid } from "@mui/x-data-grid";
 import { userColumns, userRows } from "../../datatablesource";
 import { Link } from "react-router-dom";
 
 const Datatable = () => {
+    const [data, setData] = useState(userRows);
+
+    const handleDelete = (id) => {
+        setData(data.filter(item => item.id !== id));
+    };
+
     const actionColumn = [
         {
             field: "action",
             headerName: "Action",
             width: 200,
-            renderCell: ()=> {
+            renderCell: (params) => {
                 return (
                     <div className="cellAction">
                         <Link to="/users/test" style={links}>
                             <div className="viewButton">View</div>
                         </Link>
-                        <div className="deleteButton">Delete</div>
+                        <div className="deleteButton" onClick={() => handleDelete(params.row.id)}>Delete</div>
                     </div>
                 );
             }
@@ -31,12 +37,12 @@ const Datatable = () => {
                     Add New
                 </Link>
             </div>
-            <DataGrid
-                rows={userRows}
-                columns={userColumns.concat(actionColumn)}
-                pageSize={9}
-                rowsPerPageOptions={[9]}
-                checkboxSelection
+            <DataGrid className="datagrid"
+                      rows={data}
+                      columns={userColumns.concat(actionColumn)}
+                      pageSize={9}
+                      rowsPerPageOptions={[9]}
+                      checkboxSelection
             />
         </div>
     );
@@ -44,6 +50,6 @@ const Datatable = () => {
 
 const links = {
     textDecoration: "none"
-}
+};
 
 export default Datatable;
